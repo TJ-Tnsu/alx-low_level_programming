2a1d510241a06e949db4f5d488c9e4480d604aef
Tinsae Shawel <tinsaeshawel111@gmail.com>
2022-06-07 05:51:07 -0700

    finished

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - start piont
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i = 48;
+int j = 97;
+while (i <= 57)
+{
+putchar(i);
+i++;
+}
+while (j <= 102)
+{
+putchar(j);
+j++;
+}
+putchar(10);
+return (0);
+}
