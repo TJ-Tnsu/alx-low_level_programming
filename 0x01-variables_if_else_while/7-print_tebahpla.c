c6018be051e09715f0c31f2f8e4b5da9c8c793cb
Tinsae Shawel <tinsaeshawel111@gmail.com>
2022-06-07 05:49:20 -0700

    finshed

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - start piont
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i = 122;
+while (i >= 97)
+{
+putchar(i);
+i--;
+}
+putchar(10);
+return (0);
+}
