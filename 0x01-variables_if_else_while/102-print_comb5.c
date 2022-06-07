46deee7c15c52e8c0afc1bd96873ffe93df52daa
Tinsae Shawel <tinsaeshawel111@gmail.com>
2022-06-07 06:21:59 -0700

    finished

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,40 +1,28 @@
 #include <stdio.h>
 
-/**
- * main - start piont
- *
- * Return: Always 0 (Success)
- */
 int main(void)
 {
-int i;
-int j;
-int k;
-int l;
-for (i = 48; i <= 57; i++)
-{
-for (j = 48; j <= 57; j++)
-{
-for (k = 48; k <= 57; k++)
-{
-for (l = 48; l <= 57; l++)
-{
-if (i <= k)
-{
-putchar(i);
-putchar(j);
-putchar(32);
-putchar(k);
-putchar(l);
-if (i == 57 && j == 56 && k == 57 && l == 57)
-break;
-putchar(44);
-putchar(32);
-}
-}
-}
-}
-}
-putchar(10);
-return (0);
+  int i, j;
+  for (i=0; i <100 ;i++ )
+  {
+    for (j=0; j< 100; j++)
+    {
+      if (i<j)
+      {
+        putchar((i/10) + 48);
+        putchar((i% 10)+ 48);
+        putchar( ' ');
+        putchar((j/10) + 48);
+        putchar((j%10)+ 48);
+        if (i != 98 || j !=99)
+        {
+          putchar(',');
+          putchar(' ');
+        }
+
+      }
+    }
+  }
+  putchar('\n');
+  return(0);
 }
