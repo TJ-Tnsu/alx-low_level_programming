e6e2790cbb27f8728e39fd8efa03527bf8363461
Tinsae Shawel <tinsaeshawel111@gmail.com>
2022-06-07 08:03:44 -0700

    finished

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet - prints lowercase alphabets
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+int ch;
+ch = 97;
+while (ch <= 122)
+{
+_putchar(ch);
+ch++;
+}
+_putchar('\n');
+}
