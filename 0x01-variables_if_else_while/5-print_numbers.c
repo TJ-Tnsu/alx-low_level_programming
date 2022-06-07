f1ab637e02b108efc0a283f4f5469d4635f00914
Tinsae Shawel <tinsaeshawel111@gmail.com>
2022-06-07 05:44:57 -0700

    finshed

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - start piont
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int n = 0;
+while (n < 10)
+{
+printf("%d", n);
+n++;
+}
+putchar(10);
+return (0);
+}
+
